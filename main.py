468197bfb20cda6a1694af14ef135321b7299494
gcharang <mrgcharang@gmail.com>
2019-12-05 23:56:32 +0530

    works +100

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ with open(os.path.join(dir_path, 'channels.json')) as f:
     for outFormat in outFormats:
         for categoryId, category in textChannels.items():
             dirPathCreate = os.path.join(
-                dir_path, 'output', outFormat.lower(), cleanName(category['name']))
+                './output', outFormat.lower(), cleanName(category['name']))
             for channelId, channelName in category['channels'].items():
                 exportPath = os.path.join(
                     dirPathCreate, cleanName(channelName))
