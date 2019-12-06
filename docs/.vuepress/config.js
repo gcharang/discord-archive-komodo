f2f8bf78cb802e03c4a3737cf36b2bf0006d0305
gcharang <mrgcharang@gmail.com>
2019-12-06 11:26:43 +0530

    adds logo, basic component, config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  title: "Komodo Discord Archive",
+  base: "/",
+  description: "Archive of the Komodo Discord server's public channels",
+  themeConfig: {
+    repo: "gcharang/discord-archive-komodo",
+    repoLabel: "Github",
+    docsDir: "docs",
+    editLinks: false,
+    logo: "/KMD_Horiz_White.svg"
+  }
+};
