865af61e6f2df63625632260d37933bfc882221d
gcharang <mrgcharang@gmail.com>
2019-12-08 00:46:07 +0530

    mostly works

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -5,25 +5,42 @@
     <Content class="theme-default-content" />
 
     <div v-if="formatPage">
+      <select name="Period" @change="periodDirOnchange()" v-model="selectedPeriodDirName">
+        <option disabled value>Please select the period</option>
+        <option v-for="dir in periodDirNames">{{dir}}</option>
+      </select>
+      <span>Selected: {{ selectedPeriodDirName }}</span>
+      <div v-if="dateDirs && dateDirs.length !== 0">
+        <select name="Date" @change="dateDirOnchange()" v-model="selectedDateDirName">
+          <option disabled value>Please select the date</option>
+          <option v-for="dir in dateDirNames">{{dir}}</option>
+        </select>
+        <span>Selected: {{ selectedDateDirName }}</span>
+      </div>
+      <div v-if="categoryDirs && categoryDirs.length !== 0">
+        <select name="Category" @change="categoryDirOnchange()" v-model="selectedCategoryDirName">
+          <option disabled value>Please select the category</option>
+          <option v-for="dir in categoryDirNames">{{dir}}</option>
+        </select>
+        <span>Selected: {{ selectedCategoryDirName }}</span>
+      </div>
+      <div v-if="channelDirs && channelDirs.length !== 0">
+        <select name="Channel" @change="channelOnchange()" v-model="selectedChannelDirName">
+          <option disabled value>Please select the channel</option>
+          <option v-for="dir in channelDirNames">{{dir}}</option>
+        </select>
+        <span>Selected: {{ selectedChannelDirName }}</span>
+      </div>
+      <p class="theme-default-content">{{selectedCategoryDirName}}</p>
+      <p class="theme-default-content">{{displayPaths}}</p>
       <a
         class="theme-default-content"
-        href="/before-2019-Dec-05/plaintext/ama-archive/antara-ama-july2019/1.txt"
+        v-bind:href="displayPath"
         target="_blank"
-      >/before-2019-Dec-05/plaintext/ama-archive/antara-ama-july2019/1.txt</a>
-      <p class="theme-default-content">{{timeDirChildren}}</p>
-      <select name="LeaveType" @change="timeDirOnchange()" v-model="selectedTimeDir">
-        <option disabled value>Please select one</option>
-        <option v-for="dir in timeDirNames">{{dir}}</option>
-      </select>
-      <span>Selected: {{ selectedTimeDir }}</span>
-      <div v-if="timeDirChildren.length !== 0">
-        <select name="LeaveType" @change="timeDirChildOnchange()" v-model="selectedTimeDirChild">
-          <option disabled value>Please select one</option>
-          <option v-for="dir in timeDirChildren">{{dir}}</option>
-        </select>
-        <span>Selected: {{ selectedTimeDirChild }}</span>
-      </div>
+        v-for="displayPath in displayPaths"
+      >{{displayPath}}</a>
     </div>
+
     <PageEdit />
     <PageNav v-bind="{ sidebarItems }" />
 
@@ -40,48 +57,127 @@ export default {
   props: ["sidebarItems", "staticFileStructure"],
   data: function() {
     return {
-      staticFileStructure: this.staticFileStructure,
-      selectedTimeDir: "",
-      selectedTimeDirChild: "",
-      timeDirChildren: []
+      // staticFileStructure: this.staticFileStructure,
+      selectedPeriodDirName: "",
+      selectedPeriodDir: {},
+      dateDirs: [],
+      dateDirNames: [],
+      selectedDateDirName: "",
+      selectedDateDir: {},
+      categoryDirs: [],
+      categoryDirNames: [],
+      selectedCategoryDirName: "",
+      selectedCategoryDir: {},
+      channelDirs: [],
+      channelDirNames: [],
+      selectedChannelDirName: "",
+      selectedChannelDir: {},
+      displayFiles: [],
+      displayPaths: []
     };
   },
   computed: {
     frontmatter: function() {
       return this.$page.frontmatter;
     },
-    timeDirs: function() {
+    format: function() {
+      return this.$page.frontmatter.type;
+    },
+    periodDirs: function() {
       return this.staticFileStructure.children.filter(function(dir) {
-        return dir.type == "directory";
+        return (
+          dir.type == "directory" &&
+          (dir.name.startsWith("before") || dir.name.startsWith("after"))
+        );
       });
     },
-    timeDirNames: function() {
-      return this.timeDirs.map(function(dir) {
-        return dir.name;
-      });
-    },
-    timeDirNames: function() {
-      return this.timeDirs.map(function(dir) {
+    periodDirNames: function() {
+      return this.periodDirs.map(function(dir) {
         return dir.name;
       });
     },
     formatPage: function() {
+      let vm = this;
       let formats = ["htmldark", "htmllight", "plaintext", "csv"];
-      return formats.indexOf(this.frontmatter.type) > -1;
+      return formats.indexOf(vm.frontmatter.type) > -1;
     }
   },
   methods: {
-    timeDirOnchange: function() {
+    periodDirOnchange: function() {
       let vm = this;
-      this.timeDirChildren = this.timeDirs
-        .filter(function(dir) {
-          return dir.name == vm.selectedTimeDir;
-        })[0]
-        .children.map(function(dir) {
+      let formatType = this.frontmatter.type;
+      this.selectedPeriodDir = this.periodDirs.filter(function(dir) {
+        return dir.name == vm.selectedPeriodDirName;
+      })[0];
+      if (vm.selectedPeriodDirName.startsWith("after")) {
+        this.dateDirs = this.selectedPeriodDir.children;
+        this.dateDirNames = this.dateDirs.map(function(dir) {
           return dir.name;
         });
+      } else {
+        this.categoryDirs = this.selectedPeriodDir.children.filter(function(
+          dir
+        ) {
+          return (dir.name = vm.format);
+        })[0].children;
+        this.categoryDirNames = this.categoryDirs.map(function(dir) {
+          return dir.name;
+        });
+      }
     },
-    timeDirChildOnchange: function() {}
+    dateDirOnchange: function() {
+      let vm = this;
+      this.selectedDateDir = this.selectedPeriodDir.children.filter(function(
+        dir
+      ) {
+        return dir.name == vm.selectedDateDirName;
+      })[0];
+      this.categoryDirs = this.selectedDateDir.children.filter(function(dir) {
+        return (dir.name = vm.format);
+      })[0].children;
+      this.categoryDirNames = this.categoryDirs.map(function(dir) {
+        return dir.name;
+      });
+    },
+    categoryDirOnchange: function() {
+      let vm = this;
+      if (vm.selectedPeriodDirName.startsWith("after")) {
+        this.selectedCategoryDir = this.selectedDateDir.children
+          .filter(function(dir) {
+            return (dir.name = vm.format);
+          })[0]
+          .children.filter(function(dir) {
+            return dir.name == vm.selectedCategoryDirName;
+          })[0];
+      } else {
+        this.selectedCategoryDir = this.selectedPeriodDir.children
+          .filter(function(dir) {
+            return (dir.name = vm.format);
+          })[0]
+          .children.filter(function(dir) {
+            return dir.name == vm.selectedCategoryDirName;
+          })[0];
+      }
+
+      this.channelDirs = this.selectedCategoryDir.children;
+      this.channelDirNames = this.channelDirs.map(function(dir) {
+        return dir.name;
+      });
+    },
+    channelOnchange: function() {
+      let vm = this;
+      this.selectedChannelDir = vm.channelDirs.filter(function(dir) {
+        return (dir.name = vm.selectedChannelDirName);
+      })[0];
+      this.displayFiles = this.selectedChannelDir.children;
+      this.displayPaths = this.displayFiles.map(function(dir) {
+        let dirPath = vm.selectedChannelDir.path
+          .split("/")
+          .slice(4)
+          .join("/");
+        return "/" + dirPath + "/" + dir.name;
+      });
+    }
   }
 };
 </script>
