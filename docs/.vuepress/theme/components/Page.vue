b99b7c94a0df7f285cb5496f8562973b8b0cd90a
gcharang <mrgcharang@gmail.com>
2019-12-07 15:40:52 +0530

    updates page.vue

--- docs/.vuepress/theme/components/Page.vue
+++ docs/.vuepress/theme/components/Page.vue
@@ -10,7 +10,12 @@
         href="/before-2019-Dec-05/plaintext/ama-archive/antara-ama-july2019/1.txt"
         target="_blank"
       >/before-2019-Dec-05/plaintext/ama-archive/antara-ama-july2019/1.txt</a>
-      <p class="theme-default-content">{{timeDirs}}</p>
+      <p class="theme-default-content">{{timeDirNames}}</p>
+      <select name="LeaveType" @change="timeDirOnchange()" v-model="selectedTimeDir">
+        <option disabled value>Please select one</option>
+        <option v-for="dir in timeDirNames">{{dir}}</option>
+      </select>
+      <span>Selected: {{ selectedTimeDir }}</span>
     </div>
     <PageEdit />
     <PageNav v-bind="{ sidebarItems }" />
@@ -28,7 +33,8 @@ export default {
   props: ["sidebarItems", "staticFileStructure"],
   data: function() {
     return {
-      staticFileStructure: this.staticFileStructure
+      staticFileStructure: this.staticFileStructure,
+      selectedTimeDir: ""
     };
   },
   computed: {
@@ -38,6 +44,18 @@ export default {
     timeDirs: function() {
       return this.staticFileStructure.children.filter(function(dir) {
         return dir.type == "directory";
+      });
+    },
+    timeDirNames: function() {
+      return this.timeDirs.map(function(dir) {
+        return dir.name;
+      });
+    }
+  },
+  methods: {
+    timeDirOnchange: function() {
+      this.timeDirs[this.selectedTimeDir].children.map(function(dir) {
+        return dir.name;
       });
     }
   }
