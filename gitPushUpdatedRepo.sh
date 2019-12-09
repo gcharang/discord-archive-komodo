9e16d95beb9aed5003cbaee5a3be12f577f7e443
gcharang <mrgcharang@gmail.com>
2019-12-10 02:00:38 +0530

    updates gitpush script

--- gitPushUpdatedRepo.sh
+++ gitPushUpdatedRepo.sh
@@ -3,17 +3,21 @@
 setup_git() {
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "Travis CI - gcharang"
+  echo "Email and name are set."
 }
 
 commit() {
+  git checkout master
   git add -f docs
   git commit --message "[skip ci] Travis build: $TRAVIS_BUILD_NUMBER"
+  echo "Commit is done."
 }
 
 upload_files() {
   git remote rm origin
   git remote add origin https://gcharang:${GH_TOKEN}@github.com/gcharang/discord-archive-komodo.git > /dev/null 2>&1
-  git push --quiet --set-upstream origin master 
+  git push --quiet --set-upstream origin master
+  echo "Push is done." 
 }
 
 setup_git
